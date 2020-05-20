022a1eae6d5c427c39f1251e4b1f58ad8c6b8bf7
egocentryk <lukasz.skowron@gmail.com>
2020-05-21 00:09:35 +0200

    getGamesList() method added in Game.controller.ts

--- components/game/Game.controller.ts
+++ components/game/Game.controller.ts
@@ -1,2 +1,21 @@
 import { v4 } from 'https://deno.land/std/uuid/mod.ts'
 import Game from './Game.controller.ts'
+
+let games: Game[] = [
+  {
+    id: '1',
+    name: 'Cyberpunk 2077',
+    description: 'Third person action RPG',
+    company: 'CD Project RED',
+    platform: 'PS4, PS5, Xbox One X, Xbox Series X'
+  };
+];
+
+const getGamesList = ({ response }: { response: any }) => {
+  response.body = {
+    data: {
+      list: games,
+      success: true
+    }
+  }
+}
--- components/game/Game.interface.ts
+++ components/game/Game.interface.ts
@@ -2,5 +2,6 @@ export interface Game {
   id: string;
   name: string;
   description: string;
+  company: string;
   platform: string;
 }
